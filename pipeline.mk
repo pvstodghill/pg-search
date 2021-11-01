7ff3cbe728b895830d63bcd6cb9735a281feeec2
Paul Stodghill <paul.stodghill@gmail.com>
2021-11-01 10:48:49 -0400

    Some edits ... I dunno.

--- pipeline.mk
+++ pipeline.mk
@@ -1,145 +1,154 @@
 SHELL=/bin/bash
 
-PIPELINE_DIR := $(shell realpath --relative-to=. $(dir $(lastword $(MAKEFILE_LIST))))
+ifeq ($(origin DATA), undefined)
+DATA = data
+endif
+
+
+# PIPELINE_DIR := $(shell realpath --relative-to=. $(dir $(lastword $(MAKEFILE_LIST))))
+# PIPELINE_DIRx := $(shell mkdir -p ${DATA} ; realpath --relative-to=${DATA} $(dir $(lastword $(MAKEFILE_LIST))))
+PIPELINE_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+PIPELINE_DIRx := ${PIPELINE_DIR}
 
 default:: all
 
 all::
+	@mkdir -p ${DATA}
 
 # distclean::
-# 	rm -rf data
+# 	rm -rf ${DATA}
 
 # ------------------------------------------------------------------------
 
-all:: data/genome/genome.fna
+all:: ${DATA}/genome/genome.fna
 
-data/assembly :
+${DATA}/assembly :
 	${PIPELINE_DIR}/download-assembly \
-		${ASSEMBLY_ACCESSION} data/assembly
-data/genome/genome.fna : data/assembly
+		${ASSEMBLY_ACCESSION} ${DATA}/assembly
+${DATA}/genome/genome.fna : ${DATA}/assembly
 	${PIPELINE_DIR}/demux-genome ${MOLECULE_NAMES} \
-		data/assembly data/genome
+		${DATA}/assembly ${DATA}/genome
 
 # ------------------------------------------------------------------------
 
-all:: data/sample1.mgf data/sample2.mgf data/sample3.mgf
+all:: ${DATA}/sample1.mgf ${DATA}/sample2.mgf ${DATA}/sample3.mgf
 
-data/sample1.mgf : $(SAMPLE1_MGF)
-	mkdir -p data
+${DATA}/sample1.mgf : $(SAMPLE1_MGF)
+	mkdir -p ${DATA}
 	pigz -dc $^ > $@
 
-data/sample2.mgf : $(SAMPLE2_MGF)
-	mkdir -p data
+${DATA}/sample2.mgf : $(SAMPLE2_MGF)
+	mkdir -p ${DATA}
 	pigz -dc $^ > $@
 
-data/sample3.mgf : $(SAMPLE3_MGF)
-	mkdir -p data
+${DATA}/sample3.mgf : $(SAMPLE3_MGF)
+	mkdir -p ${DATA}
 	pigz -dc $^ > $@
 
 # ------------------------------------------------------------------------
 
-all:: data/6ft.faa data/6ft.gff
+all:: ${DATA}/6ft.faa ${DATA}/6ft.gff
 
-data/6ft.faa data/6ft.gff : data/genome/genome.fna
+${DATA}/6ft.faa ${DATA}/6ft.gff : ${DATA}/genome/genome.fna
 	${PIPELINE_DIR}/make-db-6ft \
-		-f data/6ft.faa -g data/6ft.gff -s $$[3*${ORF_MIN_LENGTH}] \
-		data/genome/genome.fna
+		-f ${DATA}/6ft.faa -g ${DATA}/6ft.gff -s $$[3*${ORF_MIN_LENGTH}] \
+		${DATA}/genome/genome.fna
 
 # ------------------------------------------------------------------------
 
-all:: data/db.fasta data/db.gff
+all:: ${DATA}/db.fasta ${DATA}/db.gff
 
-data/contams.faa : ${CONTAMINANTS_DB}
-	cat ${CONTAMINANTS_DB} | tr -d '\015' > data/contams.faa
-data/contams.gff : data/contams.faa
-	cat data/contams.faa | ${PIPELINE_DIR}/make-contam-gff.pl > data/contams.gff
+${DATA}/contams.faa : ${CONTAMINANTS_DB}
+	cat ${CONTAMINANTS_DB} | tr -d '\015' > ${DATA}/contams.faa
+${DATA}/contams.gff : ${DATA}/contams.faa
+	cat ${DATA}/contams.faa | ${PIPELINE_DIR}/make-contam-gff.pl > ${DATA}/contams.gff
 
-data/db.fasta : data/6ft.faa data/contams.faa
+${DATA}/db.fasta : ${DATA}/6ft.faa ${DATA}/contams.faa
 	cat $^ > $@
-data/db.gff : data/6ft.gff data/contams.gff
+${DATA}/db.gff : ${DATA}/6ft.gff ${DATA}/contams.gff
 	cat $^ > $@
 
 
 # ------------------------------------------------------------------------
 
-all:: data/sample1.pin data/sample2.pin data/sample3.pin
+all:: ${DATA}/sample1.pin ${DATA}/sample2.pin ${DATA}/sample3.pin
 
-data/comet.params : ${COMET_PARAMS}
+${DATA}/comet.params : ${COMET_PARAMS}
 	cat ${COMET_PARAMS} \
 		| sed -e 's|DB_FILE|db.fasta|' \
-	      	> data/comet.params
+	      	> ${DATA}/comet.params
 
-data/sample1.pep.xml data/sample1.pin : data/sample1.mgf data/db.fasta data/comet.params ${PIPELINE_DIR}/packages.yaml
-	( cd data ; ../${PIPELINE_DIR}/howto -f ../${PIPELINE_DIR}/packages.yaml comet.exe sample1.mgf )
-data/sample2.pep.xml data/sample2.pin : data/sample2.mgf data/db.fasta data/comet.params ${PIPELINE_DIR}/packages.yaml
-	( cd data ; ../${PIPELINE_DIR}/howto -f ../${PIPELINE_DIR}/packages.yaml comet.exe sample2.mgf )
-data/sample3.pep.xml data/sample3.pin : data/sample3.mgf data/db.fasta data/comet.params ${PIPELINE_DIR}/packages.yaml
-	( cd data ; ../${PIPELINE_DIR}/howto -f ../${PIPELINE_DIR}/packages.yaml comet.exe sample3.mgf )
-
-# ------------------------------------------------------------------------
-
-all:: data/sample1.target_peptide_results.tsv data/sample2.target_peptide_results.tsv data/sample3.target_peptide_results.tsv
-
-data/sample1.target_peptide_results.tsv : data/sample1.pin data/db.fasta ${PIPELINE_DIR}/packages.yaml
-	${PIPELINE_DIR}/howto \
-		-f ${PIPELINE_DIR}/packages.yaml \
-		percolator data/sample1.pin \
-		--picked-protein data/db.fasta \
-		--protein-decoy-pattern DECOY_ \
-		--protein-report-fragments \
-		--protein-report-duplicates \
-		--decoy-results-psms data/sample1.decoy_psm_results.tsv \
-		--decoy-results-peptides data/sample1.decoy_peptide_results.tsv\
-		--decoy-results-proteins data/sample1.decoy_protein_results.tsv \
-		--results-psms data/sample1.target_psm_results.tsv \
-		--results-peptides data/sample1.target_peptide_results.tsv \
-		--results-proteins data/sample1.target_protein_results.tsv
-
-data/sample2.target_peptide_results.tsv : data/sample2.pin data/db.fasta ${PIPELINE_DIR}/packages.yaml
-	${PIPELINE_DIR}/howto \
-		-f ${PIPELINE_DIR}/packages.yaml \
-		percolator data/sample2.pin \
-		--picked-protein data/db.fasta \
-		--protein-decoy-pattern DECOY_ \
-		--protein-report-fragments \
-		--protein-report-duplicates \
-		--decoy-results-psms data/sample2.decoy_psm_results.tsv \
-		--decoy-results-peptides data/sample2.decoy_peptide_results.tsv\
-		--decoy-results-proteins data/sample2.decoy_protein_results.tsv \
-		--results-psms data/sample2.target_psm_results.tsv \
-		--results-peptides data/sample2.target_peptide_results.tsv \
-		--results-proteins data/sample2.target_protein_results.tsv
-
-data/sample3.target_peptide_results.tsv : data/sample3.pin data/db.fasta ${PIPELINE_DIR}/packages.yaml
-	${PIPELINE_DIR}/howto \
-		-f ${PIPELINE_DIR}/packages.yaml \
-		percolator data/sample3.pin \
-		--picked-protein data/db.fasta \
-		--protein-decoy-pattern DECOY_ \
-		--protein-report-fragments \
-		--protein-report-duplicates \
-		--decoy-results-psms data/sample3.decoy_psm_results.tsv \
-		--decoy-results-peptides data/sample3.decoy_peptide_results.tsv\
-		--decoy-results-proteins data/sample3.decoy_protein_results.tsv \
-		--results-psms data/sample3.target_psm_results.tsv \
-		--results-peptides data/sample3.target_peptide_results.tsv \
-		--results-proteins data/sample3.target_protein_results.tsv
+${DATA}/sample1.pep.xml ${DATA}/sample1.pin : ${DATA}/sample1.mgf ${DATA}/db.fasta ${DATA}/comet.params ${PIPELINE_DIR}/packages.yaml
+	( cd ${DATA} ; ${PIPELINE_DIRx}/howto -f ${PIPELINE_DIRx}/packages.yaml comet.exe sample1.mgf )
+${DATA}/sample2.pep.xml ${DATA}/sample2.pin : ${DATA}/sample2.mgf ${DATA}/db.fasta ${DATA}/comet.params ${PIPELINE_DIR}/packages.yaml
+	( cd ${DATA} ; ${PIPELINE_DIRx}/howto -f ${PIPELINE_DIRx}/packages.yaml comet.exe sample2.mgf )
+${DATA}/sample3.pep.xml ${DATA}/sample3.pin : ${DATA}/sample3.mgf ${DATA}/db.fasta ${DATA}/comet.params ${PIPELINE_DIR}/packages.yaml
+	( cd ${DATA} ; ${PIPELINE_DIRx}/howto -f ${PIPELINE_DIRx}/packages.yaml comet.exe sample3.mgf )
 
 # ------------------------------------------------------------------------
 
-all:: data/pg-search-results.gff
-data/pg-search-results.gff : data/db.fasta data/db.gff \
-		data/sample1.target_peptide_results.tsv \
-		data/sample2.target_peptide_results.tsv \
-		data/sample3.target_peptide_results.tsv \
+all:: ${DATA}/sample1.target_peptide_results.tsv ${DATA}/sample2.target_peptide_results.tsv ${DATA}/sample3.target_peptide_results.tsv
+
+${DATA}/sample1.target_peptide_results.tsv : ${DATA}/sample1.pin ${DATA}/db.fasta ${PIPELINE_DIR}/packages.yaml
+	${PIPELINE_DIR}/howto \
+		-f ${PIPELINE_DIR}/packages.yaml \
+		percolator ${DATA}/sample1.pin \
+		--picked-protein ${DATA}/db.fasta \
+		--protein-decoy-pattern DECOY_ \
+		--protein-report-fragments \
+		--protein-report-duplicates \
+		--decoy-results-psms ${DATA}/sample1.decoy_psm_results.tsv \
+		--decoy-results-peptides ${DATA}/sample1.decoy_peptide_results.tsv\
+		--decoy-results-proteins ${DATA}/sample1.decoy_protein_results.tsv \
+		--results-psms ${DATA}/sample1.target_psm_results.tsv \
+		--results-peptides ${DATA}/sample1.target_peptide_results.tsv \
+		--results-proteins ${DATA}/sample1.target_protein_results.tsv
+
+${DATA}/sample2.target_peptide_results.tsv : ${DATA}/sample2.pin ${DATA}/db.fasta ${PIPELINE_DIR}/packages.yaml
+	${PIPELINE_DIR}/howto \
+		-f ${PIPELINE_DIR}/packages.yaml \
+		percolator ${DATA}/sample2.pin \
+		--picked-protein ${DATA}/db.fasta \
+		--protein-decoy-pattern DECOY_ \
+		--protein-report-fragments \
+		--protein-report-duplicates \
+		--decoy-results-psms ${DATA}/sample2.decoy_psm_results.tsv \
+		--decoy-results-peptides ${DATA}/sample2.decoy_peptide_results.tsv\
+		--decoy-results-proteins ${DATA}/sample2.decoy_protein_results.tsv \
+		--results-psms ${DATA}/sample2.target_psm_results.tsv \
+		--results-peptides ${DATA}/sample2.target_peptide_results.tsv \
+		--results-proteins ${DATA}/sample2.target_protein_results.tsv
+
+${DATA}/sample3.target_peptide_results.tsv : ${DATA}/sample3.pin ${DATA}/db.fasta ${PIPELINE_DIR}/packages.yaml
+	${PIPELINE_DIR}/howto \
+		-f ${PIPELINE_DIR}/packages.yaml \
+		percolator ${DATA}/sample3.pin \
+		--picked-protein ${DATA}/db.fasta \
+		--protein-decoy-pattern DECOY_ \
+		--protein-report-fragments \
+		--protein-report-duplicates \
+		--decoy-results-psms ${DATA}/sample3.decoy_psm_results.tsv \
+		--decoy-results-peptides ${DATA}/sample3.decoy_peptide_results.tsv\
+		--decoy-results-proteins ${DATA}/sample3.decoy_protein_results.tsv \
+		--results-psms ${DATA}/sample3.target_psm_results.tsv \
+		--results-peptides ${DATA}/sample3.target_peptide_results.tsv \
+		--results-proteins ${DATA}/sample3.target_protein_results.tsv
+
+# ------------------------------------------------------------------------
+
+all:: ${DATA}/pg-search-results.gff
+${DATA}/pg-search-results.gff : ${DATA}/db.fasta ${DATA}/db.gff \
+		${DATA}/sample1.target_peptide_results.tsv \
+		${DATA}/sample2.target_peptide_results.tsv \
+		${DATA}/sample3.target_peptide_results.tsv \
 		${PIPELINE_DIR}/percs2gff
 	${PIPELINE_DIR}/percs2gff ${PERCS2GFF_ARGS} \
-	    -r data/perc_report.txt \
-	    -o data/pg-search-results.gff \
-	    data/db.fasta data/db.gff \
-	    data/sample1.target_peptide_results.tsv \
-	    data/sample2.target_peptide_results.tsv \
-	    data/sample3.target_peptide_results.tsv
+	    -r ${DATA}/perc_report.txt \
+	    -o ${DATA}/pg-search-results.gff \
+	    ${DATA}/db.fasta ${DATA}/db.gff \
+	    ${DATA}/sample1.target_peptide_results.tsv \
+	    ${DATA}/sample2.target_peptide_results.tsv \
+	    ${DATA}/sample3.target_peptide_results.tsv
 
 
 # ------------------------------------------------------------------------
